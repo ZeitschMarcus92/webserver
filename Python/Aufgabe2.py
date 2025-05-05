50a41fffe69fd32de1020d290ae8b2e2d22752c8
unknown <zeitschi1992@gmail.com>
2025-05-05 21:31:03 +0200

    Aufgabe 2: If-Else-Anweisungen zur Zahleneingabe umgesetzt

---
+++ Python/Aufgabe2.py
@@ -0,0 +1,12 @@
+# Aufgabe 2: If-Else-Anweisungen
+
+# Benutzer nach einer Zahl fragen
+zahl = float(input("Gib eine Zahl ein: "))
+
+# Prüfen, ob die Zahl positiv, negativ oder null ist
+if zahl > 0:
+    print("Positiv")
+elif zahl < 0:
+    print("Negativ")
+else:
+    print("Null")
